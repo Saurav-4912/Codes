efa6f7686518710c278a0bbb0f9705b050c85d3c
Saurav-4912 <sauravpawar46@gmail.com>
2024-04-09 17:55:33 +0530

    Swap Two Number

--- Swap.java
+++ Swap.java
@@ -1,10 +1,29 @@
 package Codes;
 
+import java.util.Scanner;
+
 public class Swap {
-    public static void main(String[] args){
-// System.out.println("Hello World");
-int i,j,temp;
-i=23;j=90;
-tem
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      System.out.println("Enter the first number : ");
+      int a=sc.nextInt();
+      System.out.println("Enter the second number : ");
+      int b=sc.nextInt();
+
+      int temp;
+      temp=a;
+      a=b;
+      b=temp;
+
+      System.out.println("Value of a is : " +a);
+      System.out.println("Value of b is : " +b);
+
     }
 }
+
+/*Enter the first number : 
+10
+Enter the second number : 
+12
+Value of a is : 12
+Value of b is : 10 */
