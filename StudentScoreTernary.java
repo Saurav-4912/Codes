ec43c8e131d1cee4b163a4f7bb37198db4155e7c
Saurav-4912 <sauravpawar46@gmail.com>
2024-05-03 14:40:06 +0530

    nesting of ternary operator

---
+++ StudentScoreTernary.java
@@ -0,0 +1,20 @@
+package Codes;
+
+import java.util.Scanner;
+
+public class StudentScoreTernary {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the score : ");
+        int score = sc.nextInt();
+
+        String category = score > 80 ? "High" : (score > 50) ? "Moderate" : "Low";
+        System.out.println(" Your category is :" + category);
+    }
+}
+/*
+ * output
+ * Enter the score :
+ * 50
+ * Your category is :Low
+ */
